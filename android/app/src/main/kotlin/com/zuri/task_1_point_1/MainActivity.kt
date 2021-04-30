83b00fdc1cc379a43030e07547c71f60c8cba729
Oluwafemi Okedeyi <56207395+Strangerzee@users.noreply.github.com>
2021-04-30 19:36:19 +0100

    I4GZuri Task 1.1

---
+++ android/app/src/main/kotlin/com/zuri/task_1_point_1/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.zuri.task_1_point_1
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,142 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: SafeArea(
+          child: Column(
+            children: [
+              Expanded(
+                child: Row(
+                  children: [
+                    Expanded(
+                      child: Container(
+                        child: Image.asset('assets/images/Me3.jpg'),
+                      ),
+                    ),
+                    Expanded(
+                      child: Container(
+                        child: Image.asset('assets/images/Me.jpg'),
+                      ),
+                    ),
+                    Expanded(
+                      child: Container(
+                        child: Image.asset('assets/images/Me2.jpg'),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Expanded(
+                child: Container(
+                  child: Center(
+                    child: Text(
+                      'Okedeyi Oluwafemi',
+                      style: TextStyle(
+                          color: Colors.black54,
+                          fontSize: 30,
+                          fontStyle: FontStyle.normal,
+                          fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                ),
+              ),
+              Expanded(
+                child: Container(
+                  alignment: Alignment.topLeft,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Expanded(
+                        child: Center(
+                          child: Text(
+                            'Attributes',
+                            textDirection: TextDirection.rtl,
+                            style: TextStyle(
+                              fontSize: 25,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ),
+                      ),
+                      //todo Spacer(),
+                      Expanded(
+                        child: Text(
+                          '• Tech Enthusiast',
+                          textDirection: TextDirection.ltr,
+                          textAlign: TextAlign.left,
+                          style: TextStyle(
+                            fontSize: 18,
+                            fontWeight: FontWeight.w600,
+                          ),
+                        ),
+                      ),
+                      Expanded(
+                        child: Text(
+                          '• Amateur Photographer',
+                          textDirection: TextDirection.ltr,
+                          textAlign: TextAlign.left,
+                          style: TextStyle(
+                            fontSize: 18,
+                            fontWeight: FontWeight.w600,
+                          ),
+                        ),
+                      ),
+                      Expanded(
+                        child: Text(
+                          '• Casual Gamer',
+                          textDirection: TextDirection.ltr,
+                          textAlign: TextAlign.left,
+                          style: TextStyle(
+                            fontSize: 18,
+                            fontWeight: FontWeight.w600,
+                          ),
+                        ),
+                      ),
+                      Expanded(
+                        child: Text(
+                          '• Mobile Dev',
+                          textDirection: TextDirection.ltr,
+                          textAlign: TextAlign.left,
+                          style: TextStyle(
+                            fontSize: 18,
+                            fontWeight: FontWeight.w600,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              Expanded(
+                child: Container(
+                  child: Center(
+                    child: Text(
+                      aboutMe,
+                      textDirection: TextDirection.ltr,
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.w500,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+var aboutMe = '\t A Tech Enthusiast at heart with a passion for the '
+    'mobile phone industry and a love for cameras. Testing new '
+    'boundaries in mobile app development with flutter';
